f2fcbe3d6bc2466d3f3ff91ceea27c4abb4fb568
Caleb <benJephunneh@users.noreply.github.com>
2022-07-21 15:24:02 -0400

    Update signup.ts

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -3,16 +3,16 @@ import db from "db"
 import { Signup } from "app/auth/validations"
 import { Role } from "types"
 
-export default resolver.pipe(resolver.zod(Signup), async ({ name, email, password, role }, ctx) => {
+export default resolver.pipe(resolver.zod(Signup), async ({ username, email, password, role }, ctx) => {
   const hashedPassword = await SecurePassword.hash(password.trim())
   const user = await db.user.create({
     data: {
-      name,
+      username,
       email,
       hashedPassword,
       role,
     },
-    select: { id: true, name: true, email: true, role: true },
+    select: { id: true, username: true, email: true, role: true },
   })
 
   await ctx.session.$create({ userId: user.id, role: user.role as Role })
